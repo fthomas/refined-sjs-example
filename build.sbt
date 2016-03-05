01458f7ce7b8a1fe8e23ec586c7dc5944c855f7f
Frank S. Thomas <frank@timepit.eu>
2016-03-05 19:29:35 +0100

    Persist launcher

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ name := "refined-sjs-example"
 scalaVersion := "2.11.7"
 
 libraryDependencies += "eu.timepit" %%% "refined" % "0.3.6"
+
+persistLauncher := true
--- src/main/scala/example/Test02.scala
+++ src/main/scala/example/Test02.scala
@@ -2,11 +2,13 @@ package example
 
 import eu.timepit.refined._
 import eu.timepit.refined.collection.NonEmpty
-import eu.timepit.refined.string.StartsWith
+import eu.timepit.refined.string.{ EndsWith, StartsWith, Xml }
 import scala.scalajs.js.annotation.JSExport
 
 @JSExport
 object Test02 {
   val x01 = refineMV[NonEmpty]("js")
   val x02 = refineMT[StartsWith[W.`"abc"`.T]]("abcdef")
+  val x03 = refineMV[Xml]("<elem>Hallo</elem>")
+  val x04 = refineMT[EndsWith[W.`"123456789"`.T]]("0123456789")
 }

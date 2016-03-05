dddd0fb792800c36abf2e7d677b0e27f5d4f8fac
Frank S. Thomas <frank@timepit.eu>
2016-03-05 19:54:31 +0100

    Some infer examples

---
+++ src/main/scala/example/Test03.scala
@@ -0,0 +1,16 @@
+package example
+
+import eu.timepit.refined._
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.auto._
+import eu.timepit.refined.numeric.Greater
+import scala.scalajs.js.annotation.JSExport
+import shapeless.tag.@@
+
+@JSExport
+object Test03 {
+  val x01 = refineMT[Greater[W.`10000`.T]](20000)
+  val x02: Int @@ Greater[W.`1000`.T] = x01
+  val x03 = refineMV[Greater[W.`10000`.T]](20000)
+  val x04: Int Refined Greater[W.`1000`.T] = x03
+}
